022884f3999cff319e3b8f9ba8926914625d3d9e
QuangHT <hoangthequang191294@gmail.com>
2023-07-17 21:27:27 +0700

    session 02 | method get example

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const url = require('url')
 
 const server = http.createServer((request, response) => {
+    const {pathname, query} = url.parse(request.url, true)
     response.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'text/html; charset=UTF-8'
     });
 
-    response.write('<h1>Hello Trung</h1>')
+    console.log(query)
+
+    let html = '';
+
+    html += `
+        <form action="http://127.0.0.1:8080/" method="GET">
+            <input type="text" name="keyword" placeholder="Từ khóa"/>
+            <input type="text" name="course"  placeholder="Khóa học"/>
+            <button type="submit">Tìm kiếm</button>
+        </form>
+        <p>Từ khóa: ${query.keyword}<p>
+        <p>Khóa học: ${query.course}<p>
+    `
+
+    response.write(html)
 
     response.end();
 })
