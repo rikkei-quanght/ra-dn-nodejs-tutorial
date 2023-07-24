8bea83d84dfa3c0c5e70465199f7a6ac7a07220c
QuangHT <hoangthequang191294@gmail.com>
2023-07-24 20:19:36 +0700

    session 05 | express middleware

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # Rikkei Academy - Da Nang - NodeJS Tutorial
 
-## 1. Install depenencies
+## 1. Install body-parser
+```shell
+npm install body-parser
+```
+
+## 2. Install depenencies
 ```shell
 npm install
 ```
 
-## 2. Start server
+## 3. Config
+
+
+## 4. Start server
 ```shell
 node ./src/index.js
 ```
+
+Các middlewares phổ biến khi dùng Express
+https://expressjs.com/en/resources/middleware.html
+1) body-parser (*) // parse HTTP request body
+2) morgan (*) // Ghi log (nhật ký)
+3) compression // Nén dữ liệu
+4) cors // Cross-site
+5) helmet // Helmet helps secure Express apps by setting HTTP response headers.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
 // const express = require('express');
 import express from 'express'; // Phải thêm "type": "module" ở package.json
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import fs from 'fs';
 
 const application = express();
+
+// application.use(express.json());
+// application.use(express.urlencoded());
+
+// Cấu hình body parser
+// parse application/x-www-form-urlencoded
+application.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+application.use(bodyParser.json());
+
+// Cấu hình morgan
+const accessLogStream = fs.createWriteStream('src/logs/access.log', { flags: 'a' });
+application.use(morgan('combined', { stream: accessLogStream }));
+
+
+application.get('/users', (req, res, next) => {
+    if (req.query.keyword !== undefined) {
+        next();
+    } else {
+        res.send({
+            error: 'Invalid request'
+        })
+    }
+}, (req, res) => {
+    res.send({
+        users: []
+    })
+});
+
+application.get('/users/:id', (req, res, next) => {
+    if (req.params.id === '0') {
+        next('route');
+    } else {
+        next();
+    }
+}, (req, res) => {
+    res.send({
+        id: req.params.id
+    })
+})
+
+application.post('/users', (req, res) => {
+    res.send({
+        requestBody: req.body
+    })
+});
 
 application.listen(8000, () => {
     console.log('Server started');
